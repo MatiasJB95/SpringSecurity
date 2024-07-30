dd638538bc6d6713fbf5de726642ccb88cbb83ee
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-07-29 23:21:20 -0300

    Update Product.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/persistence/entity/Product.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/persistence/entity/Product.java
@@ -5,9 +5,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.math.BigDecimal;
+
 @Entity
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
+    private BigDecimal price;
+    private ProductStatus status;
+
+    public static enum ProductStatus {
+        ENABLE, DISABLE;
+    }
 }
