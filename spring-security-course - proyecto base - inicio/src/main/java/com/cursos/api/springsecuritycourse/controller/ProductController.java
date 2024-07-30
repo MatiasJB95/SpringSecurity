ecdf3385c0c417e6fb66edf20ab606c2d38db8d0
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-07-29 23:15:49 -0300

    Update ProductController.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/controller/ProductController.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/controller/ProductController.java
@@ -1,9 +1,16 @@
 package com.cursos.api.springsecuritycourse.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    @GetMapping
+    public ResponseEntity<Page<Product>>findAll(Pageable pageable)
 }
