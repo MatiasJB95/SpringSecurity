b743b0312108505eacbf211785e9bfd864df6288
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-07-29 23:31:48 -0300

    Update ProductController.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/controller/ProductController.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/controller/ProductController.java
@@ -1,21 +1,36 @@
 package com.cursos.api.springsecuritycourse.controller;
 
+import com.cursos.api.springsecuritycourse.persistence.entity.Product;
+import com.cursos.api.springsecuritycourse.service.ProductService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    private ProductService productService;
 
     @GetMapping
     public ResponseEntity<Page<Product>>findAll(Pageable pageable){
         Page<Product> productPage =productService.findAll(pageable);
         if (productPage.hasContent()){
             return ResponseEntity.ok(productPage);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product>findOneById(@PathVariable Long productId){
+        Optional<Product> product = productService.findByOneId(productId);
+        if (product.isPresent()){
+            return ResponseEntity.ok(product.get());
         }
         return ResponseEntity.notFound().build();
     }
