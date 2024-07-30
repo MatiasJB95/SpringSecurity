1297fdbd88bc4ca827e1bf6d6945805df8c1b82c
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-07-30 20:50:32 -0300

    Update ProductController.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/controller/ProductController.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/controller/ProductController.java
@@ -5,10 +5,7 @@ import com.cursos.api.springsecuritycourse.service.ProductService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -28,6 +25,14 @@ public class ProductController {
 
     @GetMapping("/{productId}")
     public ResponseEntity<Product>findOneById(@PathVariable Long productId){
+        Optional<Product> product = productService.findByOneId(productId);
+        if (product.isPresent()){
+            return ResponseEntity.ok(product.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @PostMapping ("/{productId}")
+    public ResponseEntity<Product>findOneById(SaveProduct saveProduct){
         Optional<Product> product = productService.findByOneId(productId);
         if (product.isPresent()){
             return ResponseEntity.ok(product.get());
