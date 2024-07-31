26b884a6504182904fbc289b2359b92635df6c5b
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-07-31 20:59:12 -0300

    Create CategoryService.java

---
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/service/CategoryService.java
@@ -0,0 +1,14 @@
+package com.cursos.api.springsecuritycourse.service;
+
+import com.cursos.api.springsecuritycourse.persistence.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface CategoryService {
+
+    Page<Category> findAll(Pageable pageable);
+
+
+}
