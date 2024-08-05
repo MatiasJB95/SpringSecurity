17ab085c62d758225ec8145c627eb3dba5b3912a
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-05 20:20:19 -0300

    Update SaveCategory.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/dto/SaveCategory.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/dto/SaveCategory.java
@@ -8,4 +8,11 @@ public class SaveCategory implements Serializable {
     @NotBlank
     private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
