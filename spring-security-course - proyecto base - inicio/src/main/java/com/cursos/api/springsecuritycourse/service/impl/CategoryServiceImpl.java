60a7637b16be8b95acbb13e909b26503a5c2d97d
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-05 20:16:54 -0300

    Update CategoryServiceImpl.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/service/impl/CategoryServiceImpl.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/service/impl/CategoryServiceImpl.java
@@ -1,4 +1,42 @@
 package com.cursos.api.springsecuritycourse.service.impl;
 
-public class CategoryServiceImpl {
+import com.cursos.api.springsecuritycourse.dto.SaveCategory;
+import com.cursos.api.springsecuritycourse.persistence.entity.Category;
+import com.cursos.api.springsecuritycourse.persistence.repository.CategoryRepository;
+import com.cursos.api.springsecuritycourse.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Override
+    public Page<Category> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Optional<Category> findByOneId(Long categoryId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Category createOn(SaveCategory saveCategory) {
+        return null;
+    }
+
+    @Override
+    public Category updateOneById(Long categoryId, SaveCategory saveCategory) {
+        return null;
+    }
+
+    @Override
+    public Category disableOneById(Long categoryId) {
+        return null;
+    }
 }
