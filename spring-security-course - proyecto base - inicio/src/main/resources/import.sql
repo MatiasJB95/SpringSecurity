3faf3eb7fe4e5a5f51093dde7115c4b0bedcb39e
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-06 19:59:27 -0300

    Update import.sql

--- spring-security-course - proyecto base - inicio/src/main/resources/import.sql
+++ spring-security-course - proyecto base - inicio/src/main/resources/import.sql
@@ -1,10 +1,8 @@
--- CREACIÓN DE CATEGORIAS
+
 INSERT INTO category (name, status) VALUES ('Electrónica', 'ENABLED');
 INSERT INTO category (name, status) VALUES ('Ropa', 'ENABLED');
 INSERT INTO category (name, status) VALUES ('Deportes', 'ENABLED');
 INSERT INTO category (name, status) VALUES ('Hogar', 'ENABLED');
-
--- CREACIÓN DE PRODUCTOS
 INSERT INTO product (name, price, status, category_id) VALUES ('Smartphone', 500.00, 'ENABLED', 1);
 INSERT INTO product (name, price, status, category_id) VALUES ('Auriculares Bluetooth', 50.00, 'DISABLED', 1);
 INSERT INTO product (name, price, status, category_id) VALUES ('Tablet', 300.00, 'ENABLED', 1);
