24ebdf46a573401e362492ac5a627af7189b9b25
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-07 19:09:11 -0300

    Update GlobalExceptionHandler.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/exception/GlobalExceptionHandler.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.cursos.api.springsecuritycourse.dto.ApiError;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpServerErrorException;
@@ -18,5 +19,16 @@ public class GlobalExceptionHandler {
         apiError.setMethod(request.getMethod());
         apiError.setMessage("Error interno en el servidor, vuelva mas tarde");
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handlerMethodArgumentNotValidException(HttpServletRequest request,
+                                                                    MethodArgumentNotValidException exception) {
+        ApiError apiError = new ApiError();
+        apiError.setBackendMessage(exception.getLocalizedMessage());
+        apiError.setUrl(request.getRequestURL().toString());
+        apiError.setMethod(request.getMethod());
+        apiError.setMessage("Error en la peticion enviada.");
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
     }
 }
