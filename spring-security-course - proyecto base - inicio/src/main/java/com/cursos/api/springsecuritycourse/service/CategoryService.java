3a4155b4a40fd2cd1f4b5abe9f238a88381cc2b5
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-07 18:38:56 -0300

    Update CategoryService.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/service/CategoryService.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/service/CategoryService.java
@@ -5,9 +5,10 @@ import com.cursos.api.springsecuritycourse.persistence.entity.Category;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Service
 public interface CategoryService {
 
     Page<Category> findAll(Pageable pageable);
@@ -21,4 +22,5 @@ public interface CategoryService {
 
     Category disableOneById(Long categoryId);
 
+    Category enabledOneById(Long categoryId);
 }
