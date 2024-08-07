9862d3e8d02beb5f086e23c4f810905209aad1ea
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-07 19:05:52 -0300

    Update ApiError.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/dto/ApiError.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/dto/ApiError.java
@@ -6,6 +6,8 @@ import java.time.LocalDateTime;
 public class ApiError implements Serializable {
     private String backendMessage;
     private String message;
+    private String url;
+    private String method;
     private LocalDateTime timeStamp;
 
     public String getBackendMessage() {
@@ -30,5 +32,21 @@ public class ApiError implements Serializable {
 
     public void setTimeStamp(LocalDateTime timeStamp) {
         this.timeStamp = timeStamp;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
     }
 }
