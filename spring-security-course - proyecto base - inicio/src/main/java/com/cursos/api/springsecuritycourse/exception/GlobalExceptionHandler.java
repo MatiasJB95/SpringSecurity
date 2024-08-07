6ab4cce47bb11a5b2e3e585b42f09753ecfc8831
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-07 19:05:54 -0300

    Update GlobalExceptionHandler.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/exception/GlobalExceptionHandler.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/exception/GlobalExceptionHandler.java
@@ -1,13 +1,22 @@
 package com.cursos.api.springsecuritycourse.exception;
 
+import com.cursos.api.springsecuritycourse.dto.ApiError;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpServerErrorException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handlerGenericException() {
-        
+    public ResponseEntity<?> handlerGenericException(HttpServletRequest request, Exception exception) {
+        ApiError apiError = new ApiError();
+        apiError.setBackendMessage(exception.getLocalizedMessage());
+        apiError.setUrl(request.getRequestURL().toString());
+        apiError.setMethod(request.getMethod());
+        apiError.setMessage("Error interno en el servidor, vuelva mas tarde");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
     }
 }
