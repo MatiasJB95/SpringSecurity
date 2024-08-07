379268d1ee8ff84776e2c59f92e2b2e40e7a187a
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-07 18:54:19 -0300

    Create GlobalExceptionHandler.java

---
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.cursos.api.springsecuritycourse.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+}
