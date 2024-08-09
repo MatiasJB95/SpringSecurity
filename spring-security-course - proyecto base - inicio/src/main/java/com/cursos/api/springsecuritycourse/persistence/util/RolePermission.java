04959598a10b25bccf77fc14875d2ac6a8adfc3e
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-09 18:58:01 -0300

    Create RolePermission.java

---
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/persistence/util/RolePermission.java
@@ -0,0 +1,9 @@
+package com.cursos.api.springsecuritycourse.persistence.util;
+
+public enum RolePermission {
+    READ_ALL_PRODUCTS,
+    READ_ONE_PRODUCT,
+    CREATE_ONE_PRODUCT,
+    UPDATE_ONE_PRODUCT,
+   DISABLED_ONE_PRODUCT
+}
