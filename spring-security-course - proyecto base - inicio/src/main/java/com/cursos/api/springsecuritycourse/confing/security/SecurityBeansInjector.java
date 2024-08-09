163e020bda56ba6c9628918dc763c9abc6c2e5cc
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-08 23:20:24 -0300

    Create SecurityBeansInjector.java

---
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/confing/security/SecurityBeansInjector.java
@@ -0,0 +1,8 @@
+package com.cursos.api.springsecuritycourse.confing.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityBeansInjector {
+    
+}
