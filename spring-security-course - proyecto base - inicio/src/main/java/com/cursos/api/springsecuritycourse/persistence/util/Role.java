c1f4f131a48be00f75f601463fd50196627b1938
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-09 19:12:48 -0300

    Update Role.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/persistence/util/Role.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/persistence/util/Role.java
@@ -2,13 +2,14 @@ package com.cursos.api.springsecuritycourse.persistence.util;
 
 
 import java.util.Arrays;
+import java.util.List;
 
 public enum Role {
     ROLE_ADMINISTRATOR(Arrays.asList(
             RolePermission.READ_ALL_PRODUCTS,
             RolePermission.READ_ONE_PRODUCT,
             RolePermission.CREATE_ONE_PRODUCT,
-            RolePermission.UPDATE_ONE_PRODUCT,,
+            RolePermission.UPDATE_ONE_PRODUCT,
             RolePermission.DISABLED_ONE_PRODUCT,
 
             RolePermission.READ_ALL_CATEGORIES,
@@ -16,15 +17,13 @@ public enum Role {
             RolePermission.CREATE_ONE_CATEGORY,
             RolePermission.UPDATE_ONE_CATEGORY,
             RolePermission.DISABLED_ONE_CATEGORY,
-
-
             RolePermission.READ_MY_PROFILE
 
     )),
     ROLE_ASSISTENT_ADRMINISTRATOR(Arrays.asList(
             RolePermission.READ_ALL_PRODUCTS,
             RolePermission.READ_ONE_PRODUCT,
-            RolePermission.UPDATE_ONE_PRODUCT,,
+            RolePermission.UPDATE_ONE_PRODUCT,
 
             RolePermission.READ_ALL_CATEGORIES,
             RolePermission.READ_ONE_CATEGORY,
@@ -36,8 +35,18 @@ public enum Role {
     )),
     ROLE_CUSTOMER(Arrays.asList(
             RolePermission.READ_MY_PROFILE
-    )),;
-    
+    ));
+private List<RolePermission> permissions;
 
+    Role(List<RolePermission> permissions) {
+        this.permissions = permissions;
+    }
 
+    public List<RolePermission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<RolePermission> permissions) {
+        this.permissions = permissions;
+    }
 }
