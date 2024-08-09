526eccf54c87250078b5c395f7b3cea90d45fbb1
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-08 23:44:44 -0300

    Update SecurityBeansInjector.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/confing/security/SecurityBeansInjector.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/confing/security/SecurityBeansInjector.java
@@ -4,18 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @Configuration
 public class SecurityBeansInjector {
 
-    @Autowired
-    private AuthenticationConfiguration authenticationConfiguration;
-
-
     @Bean
-    public AuthenticationManager authenticationManager() throws Exception {
-        authenticationConfiguration.getAuthenticationManager();
-
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationStrategy
+    }
+
 }
