f2f4524e446def3aeb5cef9d15588b250748f07b
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-08 23:21:01 -0300

    Update SecurityBeansInjector.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/confing/security/SecurityBeansInjector.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/confing/security/SecurityBeansInjector.java
@@ -2,9 +2,12 @@ package com.cursos.api.springsecuritycourse.confing.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 
 @Configuration
 public class SecurityBeansInjector {
     @Bean
-    
+   public AuthenticationManager authenticationManager() {
+        
+    }
 }
