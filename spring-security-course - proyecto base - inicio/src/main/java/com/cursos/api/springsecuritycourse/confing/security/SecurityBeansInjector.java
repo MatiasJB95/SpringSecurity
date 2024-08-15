b46802e9c3d5e6c12a33091bb11ef670866fa9d5
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-15 02:25:32 -0300

    Update SecurityBeansInjector.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/confing/security/SecurityBeansInjector.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/confing/security/SecurityBeansInjector.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -26,7 +27,7 @@ public class SecurityBeansInjector {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return 
+        return new BCryptPasswordEncoder();
     }
 }
 
