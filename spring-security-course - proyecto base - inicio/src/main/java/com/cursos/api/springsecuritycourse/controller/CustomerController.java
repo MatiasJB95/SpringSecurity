9f9e56c66b2c35a0180916a937e9904eecad7e4a
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-15 23:05:48 -0300

    Update CustomerController.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/controller/CustomerController.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/controller/CustomerController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.cursos.api.springsecuritycourse.service.auth.AuthenticationService;
+import com.cursos.api.springsecuritycourse.dto.RegisteredUser;
 
 
 @RestController
