2194a33d4c54e63f48562fb8f4fdb49388c19b75
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-15 23:14:02 -0300

    Update SaveUser.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/dto/SaveUser.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/dto/SaveUser.java
@@ -16,4 +16,36 @@ public class SaveUser implements Serializable {
     private String password;
     @Size(min=8)
     private String repeatPassword;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
+    }
 }
