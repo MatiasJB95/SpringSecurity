bef0f3152bc4e74bb3a1679fbc09688d837d9912
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-15 22:59:16 -0300

    Update CustomerController.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/controller/CustomerController.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/controller/CustomerController.java
@@ -2,8 +2,24 @@ package com.cursos.api.springsecuritycourse.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
+@RequestMapping("/Customers")
 public class CustomerController {
 
+    @PostMapping()
+    public ResponseEntity <RegisteredUser> registerOne(@RequestBody @Valid
+        SaveUser newUser) {
+            RegisteredUser = authenticateService.registerOneCustomer(newUser);
+
+ 
+    }
     
 }
