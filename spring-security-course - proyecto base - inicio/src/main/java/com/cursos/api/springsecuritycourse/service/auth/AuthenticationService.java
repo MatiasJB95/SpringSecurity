e3c7794908097d2b2f3b892ca66f10452f69cc74
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-15 23:24:34 -0300

    Update AuthenticationService.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/service/auth/AuthenticationService.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/service/auth/AuthenticationService.java
@@ -1,12 +1,13 @@
 package com.cursos.api.springsecuritycourse.service.auth;
 
+import com.cursos.api.springsecuritycourse.dto.RegisteredUser;
 import com.cursos.api.springsecuritycourse.dto.SaveUser;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
 
-    public Object registerOneCustomer(SaveUser newUser) {
+    public RegisteredUser registerOneCustomer(SaveUser newUser) {
         return null;
     }
 }
