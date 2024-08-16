2a78fbaedab13d3a02e27900da8b9f38c434af17
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-15 22:54:56 -0300

    Create CustomerController.java

---
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/controller/CustomerController.java
@@ -0,0 +1,9 @@
+package com.cursos.api.springsecuritycourse.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+    
+}
