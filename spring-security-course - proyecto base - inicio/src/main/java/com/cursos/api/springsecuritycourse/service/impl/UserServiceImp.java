55a922d59bbe147797b22fd05edb47c9e91b732a
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-24 00:03:06 -0300

    Update UserServiceImp.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/service/impl/UserServiceImp.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/service/impl/UserServiceImp.java
@@ -1,11 +1,14 @@
 package com.cursos.api.springsecuritycourse.service.impl;
 
 import com.cursos.api.springsecuritycourse.dto.SaveUser;
+import com.cursos.api.springsecuritycourse.exception.InvalidPasswordException;
 import com.cursos.api.springsecuritycourse.persistence.entity.User;
 import com.cursos.api.springsecuritycourse.persistence.repository.UserRepository;
+import com.cursos.api.springsecuritycourse.persistence.util.Role;
 import com.cursos.api.springsecuritycourse.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 public class UserServiceImp implements UserService {
@@ -14,6 +17,23 @@ public class UserServiceImp implements UserService {
     private UserRepository userRepository;
     @Override
     public User registerOneCustomer(SaveUser newUser) {
-        return null;
+        validatePassword(newUser);
+
+        User user = new User();
+        user.setUsername(newUser.getUsername());
+        user.setPassword(newUser.getPassword();
+        user.setName(newUser.getName());
+        user.setRole(Role.ROLE_CUSTOMER);
+
+        return userRepository.save(user);
+    }
+
+    private void validatePassword(SaveUser dto) {
+        if(!StringUtils.hasText(dto.getPassword())|| !StringUtils.hasText(dto.getRepeatPassword())){
+            throw new InvalidPasswordException("No coinciden las contraseñas");
+        }
+        if(!dto.getPassword().equals(dto.getRepeatPassword())){
+            throw new InvalidPasswordException("No coinciden las contraseñas");
+        }
     }
 }
