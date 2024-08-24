285ba4ec928cd41049bec3f8d980c9bbfae456aa
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-23 23:50:54 -0300

    Update JwtService.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/service/auth/JwtService.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/service/auth/JwtService.java
@@ -1,7 +1,12 @@
 package com.cursos.api.springsecuritycourse.service.auth;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+
+    public String generateToken(UserDetails user) {
+
+    }
 }
