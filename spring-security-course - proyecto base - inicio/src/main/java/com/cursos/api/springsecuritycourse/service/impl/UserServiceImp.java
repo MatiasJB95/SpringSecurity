42f8e69b4a4017ee9f1feefe2f2a13bf3a4753b4
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2024-08-23 23:54:40 -0300

    Update UserServiceImp.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/service/impl/UserServiceImp.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/service/impl/UserServiceImp.java
@@ -2,11 +2,16 @@ package com.cursos.api.springsecuritycourse.service.impl;
 
 import com.cursos.api.springsecuritycourse.dto.SaveUser;
 import com.cursos.api.springsecuritycourse.persistence.entity.User;
+import com.cursos.api.springsecuritycourse.persistence.repository.UserRepository;
 import com.cursos.api.springsecuritycourse.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImp implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public User registerOneCustomer(SaveUser newUser) {
         return null;
