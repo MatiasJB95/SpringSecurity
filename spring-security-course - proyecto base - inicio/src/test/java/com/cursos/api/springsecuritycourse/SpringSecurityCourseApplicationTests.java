d8a42de7441f831738f5ca83a02c347e7e237a55
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-03-13 12:23:03 -0300

    Actualizar SpringSecurityCourseApplicationTests.java

--- spring-security-course - proyecto base - inicio/src/test/java/com/cursos/api/springsecuritycourse/SpringSecurityCourseApplicationTests.java
+++ spring-security-course - proyecto base - inicio/src/test/java/com/cursos/api/springsecuritycourse/SpringSecurityCourseApplicationTests.java
@@ -1,4 +1,4 @@
-1package com.cursos.api.springsecuritycourse;
+21package com.cursos.api.springsecuritycourse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
