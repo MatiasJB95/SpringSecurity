3b429aebcabdae29aaa3dbb8ad3eb889c21745fb
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-07-16 07:49:19 -0300

    Actualizar SpringSecurityCourseApplication.java

--- spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/SpringSecurityCourseApplication.java
+++ spring-security-course - proyecto base - inicio/src/main/java/com/cursos/api/springsecuritycourse/SpringSecurityCourseApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSecurityCourseApplication {
 
 	public static void main(String[] args) {
-	12345	SpringApplication.run(SpringSecurityCourseApplication.class, args);
+	1234	SpringApplication.run(SpringSecurityCourseApplication.class, args);
 	}
 
 }
